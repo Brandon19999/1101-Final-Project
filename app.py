2a6f8ce3c60c8632bad607a657113c5c3f8b4d0f
Brandon <Samurai_Blitz18@yahoo.com>
2023-11-29 09:10:14 -0600

    11/29

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-# My first commit
-print("Hello World")
-# New comment
+# import libraries and packages, they contain additional functions.
+
+from flask import Flask, render_template
+
+
+#create web application usin Flask
+app = Flask(__name__)
+
+#create our routes
+@app.route('/')
+def home():
+ return render_template('index.html')
+
+#start the web application 
+app.run(
+ debug=True
+)
